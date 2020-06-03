05fa70cbdd9f619cfe4acfc351333f57c71ccb1d
“parepallirevanth” <parepallirevanth@gmail.com>
2020-06-03 17:47:53 +0530

    updated scripts

--- scripts/gunicorn.sh
+++ scripts/gunicorn.sh
@@ -2,7 +2,7 @@
 
 #gunicorn --bind 0.0.0.0:8000 chatapp.wsgi:application
 
-sudo cp /home/Revanth/Chatapplication/scripts/gunicorn.service /etc/systemd/system/
+sudo cp /home/$USER/chatappAutomation/scripts/gunicorn.service /etc/systemd/system/
 sudo systemctl daemon-reload
 sudo systemctl start gunicorn
 sudo systemctl enable gunicorn
--- scripts/installscript.sh
+++ scripts/installscript.sh
@@ -14,10 +14,10 @@ pip3 install virtualenv
 # Creating Virtual env and activate
 cd /home/Revanth/Chatapplication
 virtualenv --python=python3 venv
-source /home/Revanth/Chatapplication/venv/bin/activate
+source /home/$USER/chatappAutomation/venv/bin/activate
 
 # Installing Requirements
-cd  /home/Revanth/Chatapplication/chatapplication
+cd  /home/$USER/chatappAutomation/chatapplication
 pip3 install -r requirements.txt
 pip3 install django bcrypt django-extensions
 
@@ -28,7 +28,7 @@ python3 manage.py collectstatic --noinput
 
 
 # nginx configuration
-sudo cp /home/Revanth/Chatapplication/scripts/nginx /etc/nginx/sites-available/
+sudo cp /home/$USER/chatappAutomation/scripts/nginx /etc/nginx/sites-available/
 sudo ln -s /etc/nginx/sites-available/chatapplication /etc/nginx/sites-enabled/
 # checking ngnix syntax
 sudo nginx -t
